84405bd981b4eaefc402d6c8ab3e9c3ff70008f2
Tanav Poswal <tanavposwal939@gmail.com>
2025-10-07 20:56:06 +0530

    get user input

--- main.cpp
+++ main.cpp
@@ -6,57 +6,88 @@ using namespace std;
 // black - capital
 
 char board[8][8] = {
-        {'R', 'N', 'B', 'Q', 'K', 'B', 'N', 'R'},
-        {'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P'},
-        {'.', '.', '.', '.', '.', '.', '.', '.'},
-        {'.', '.', '.', '.', '.', '.', '.', '.'},
-        {'.', '.', '.', '.', '.', '.', '.', '.'},
-        {'.', '.', '.', '.', '.', '.', '.', '.'},
-        {'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p'},
-        {'r', 'n', 'b', 'q', 'k', 'b', 'n', 'r'}
-    };
+    {'R', 'N', 'B', 'Q', 'K', 'B', 'N', 'R'},
+    {'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P'},
+    {'.', '.', '.', '.', '.', '.', '.', '.'},
+    {'.', '.', '.', '.', '.', '.', '.', '.'},
+    {'.', '.', '.', '.', '.', '.', '.', '.'},
+    {'.', '.', '.', '.', '.', '.', '.', '.'},
+    {'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p'},
+    {'r', 'n', 'b', 'q', 'k', 'b', 'n', 'r'}};
 
-void board_cout(char board[8][8]) {
-	cout << "+-----------------+" << endl;
-	for (int i = 0; i < 8; i++) {
-		// border
-		cout << "| ";
+void board_cout(char board[8][8])
+{
+    cout << endl
+         << " +-----------------+" << endl;
+    for (int i = 0; i < 8; i++)
+    {
+        // border
+        cout << (8 - i);
+        cout << "| ";
 
-		for (int j = 0; j < 8; j++) {
-			if (board[i][j] != '.') {
-				cout << board[i][j] << " ";
-			} else {
-				cout << "  ";
-			}
-		}
+        for (int j = 0; j < 8; j++)
+        {
+            if (board[i][j] != '.')
+            {
+                cout << board[i][j] << " ";
+            }
+            else
+            {
+                cout << "  ";
+            }
+        }
 
-		cout << "|" << endl;
-	}
-	cout << "+-----------------+" << endl;
+        cout << "|" << endl;
+    }
+    cout << " +-----------------+" << endl;
+    cout << "   a b c d e f g h" << endl
+         << endl;
 }
 
-pair<int, int> findCoord(string point) {
-    int col = '8'-point[1];
-    int row = point[0]-'a';
+pair<int, int> findCoord(string point)
+{
+    int col = '8' - point[1];
+    int row = point[0] - 'a';
 
     return {col, row};
 }
 
-void move_piece(string from, string to) {
+void move_piece(string from, string to)
+{
     // TODO: validate move
     pair<int, int> f = findCoord(from);
     pair<int, int> t = findCoord(to);
-    if (board[f.first][f.second] != '.') {
+    if (board[f.first][f.second] != '.')
+    {
         board[t.first][t.second] = board[f.first][f.second];
         board[f.first][f.second] = '.';
     }
 }
 
-int main() {
-    
-	// move pawn
-    move_piece("a2", "a4");
-	board_cout(board);
+int main()
+{
 
-	return 0;
+    bool run = true;
+
+    while (run)
+    {
+        board_cout(board);
+
+        cout << "---- YOURs TURN (q q to quit) ----" << endl;
+
+        string from;
+        string to;
+        cout << ">> ";
+        cin >> from >> to;
+
+        if (from == "q" && to == "q")
+        {
+            run = false;
+            break;
+        }
+
+        move_piece(from, to);
+    }
+
+    return 0;
 }
